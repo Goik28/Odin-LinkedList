24cb5b82e5cc4ca72a2731f7bea068ea12ea5da1
Goik <sbabinski@outlook.com>
2022-08-12 14:58:30 -0300

    Init project
    Create files.
    Write Readme.
    Start script structure.

---
+++ script.js
@@ -0,0 +1,21 @@
+class LinkedList {
+  head = null;
+  tail = null;
+  size = null;
+  append(node) {}
+  prepend(node) {}
+  pop() {}
+  at(index) {
+    return;
+  }
+  contains(node) {}
+  find(node) {}
+  toString() {}
+  insertAt(node, index) {}
+  removeAt(index) {}
+}
+
+class LinkedNode {
+  value = null;
+  nextNode = null;
+}
