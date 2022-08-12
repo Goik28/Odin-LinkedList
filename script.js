06709fb7be8c761032d52c7b88cefab74b046513
Goik <sbabinski@outlook.com>
2022-08-12 17:07:43 -0300

    Implement some methods
    Implements append, prepend, pop and at.

--- script.js
+++ script.js
@@ -2,15 +2,64 @@ class LinkedList {
   head = null;
   tail = null;
   size = null;
-  append(node) {}
-  prepend(node) {}
-  pop() {}
-  at(index) {
-    return;
+
+  constructor(node) {
+    this.head = node;
+    this.tail = node;
+    this.size = 1;
   }
-  contains(node) {}
-  find(node) {}
-  toString() {}
+
+  createNode(value) {
+    return new LinkedNode(value);
+  }
+
+  append(value) {
+    this.tail.nextNode = this.createNode(value);
+    this.tail = tail.nextNode;
+    this.size++;
+  }
+
+  prepend(value) {
+    const newNode = this.createNode(value);
+    newNode.nextNode = head;
+    head = newNode;
+    this.size++;
+  }
+
+  pop() {
+    let current;
+    current = this.head;
+    while (current.nextNode != this.tail) {
+      current = current.nextNode;
+    }
+    this.tail = current;
+    current.nextNode = null;
+    this.size--;
+  }
+
+  at(index) {
+    let current;
+    current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.nextNode;
+    }
+    return current;
+  }
+
+  contains(value) {}
+
+  find(value) {}
+
+  toString() {
+    let current;
+    let string = "First: ";
+    current = this.head;
+    do {
+      string.concat(current.value + " -> ");
+    } while (current.nextNode != null);
+    string.concat(" null");
+    return string;
+  }
   insertAt(node, index) {}
   removeAt(index) {}
 }
@@ -18,4 +67,8 @@ class LinkedList {
 class LinkedNode {
   value = null;
   nextNode = null;
+
+  constructor(value) {
+    this.value = value;
+  }
 }
